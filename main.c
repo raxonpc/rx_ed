32e576cf602be1b874ad3fa43234f41d1a09248e
raxonpc <tomeczekorlowski1@gmail.com>
2022-04-10 23:48:20 +0200

    Initial commit, access input in raw mode

---
+++ main.c
@@ -0,0 +1,61 @@
+#include <unistd.h>
+#include <termios.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/*** DATA ***/
+struct termios orig_termios;
+
+/*** TERMINAL ***/
+void die(const char *s)
+{
+    perror(s);
+    exit(1);
+}
+
+void disable_raw_mode()
+{
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+        die("tcsetattr");
+}
+
+void enable_raw_mode()
+{
+    if(tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+        die("tcgetattr");
+    atexit(disable_raw_mode);
+
+    struct termios raw = orig_termios;
+
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 20;
+
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == 1)
+        die("tcsetattr");
+}
+
+/*** INIT ***/
+int main()
+{
+    enable_raw_mode();
+
+    while(1) {
+        char c = '\0';
+        if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
+            die("read");
+        if (iscntrl(c)) {
+            printf("%d\r\n", c);
+        } else {
+            printf("%d (\'%c\')\r\n", c, c);
+        }
+        if (c == 'q') break;
+    }
+
+    return 0;
+}
