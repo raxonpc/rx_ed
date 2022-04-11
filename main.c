9cf8438b05816567752c67912f34553249ba8280
raxonpc <tomeczekorlowski1@gmail.com>
2022-04-11 15:00:25 +0200

    Get terminal size, write tilde at the beginning of each line

--- main.c
+++ main.c
@@ -4,30 +4,44 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <sys/ioctl.h>
+
+/*** DEFINES ***/
+#define CTRL_KEY(k) ((k) & 0x1f)
 
 /*** DATA ***/
-struct termios orig_termios;
+struct editor_config
+{
+    struct termios orig_termios;
+    int screen_cols;
+    int screen_rows;
+};
+
+struct editor_config ed_cfg;
 
 /*** TERMINAL ***/
 void die(const char *s)
 {
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
     perror(s);
     exit(1);
 }
 
 void disable_raw_mode()
 {
-    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &ed_cfg.orig_termios) == -1)
         die("tcsetattr");
 }
 
 void enable_raw_mode()
 {
-    if(tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+    if(tcgetattr(STDIN_FILENO, &ed_cfg.orig_termios) == -1)
         die("tcgetattr");
     atexit(disable_raw_mode);
 
-    struct termios raw = orig_termios;
+    struct termios raw = ed_cfg.orig_termios;
 
     raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
     raw.c_oflag &= ~(OPOST);
@@ -40,21 +54,114 @@ void enable_raw_mode()
         die("tcsetattr");
 }
 
+char editor_read_key()
+{
+    ssize_t nread;
+    char c;
+
+    while((nread = read(STDIN_FILENO, &c, 1)) != 1)
+    {
+        if(nread == -1 && errno != EAGAIN) die("read");
+    }
+    return c;
+}
+
+int get_cursor_position(int *cols, int *rows)
+{
+    char buf[32];
+    unsigned int i = 0;
+    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+    while (i < sizeof(buf) - 1)
+    {
+        if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+        if (buf[i++] == 'R') break;
+    }
+    buf[i] = '\0';
+
+    if(buf[0] != '\x1b' || buf[1] != '[') return -1;
+    if(sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+
+    return 0;
+}
+
+int get_window_size(int *cols, int *rows)
+{
+    struct winsize ws;
+
+    if(ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
+    {
+        if(write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12)
+            return -1;
+
+        return get_cursor_position(cols, rows);
+    }
+    else
+    {
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
+}
+
+/*** OUTPUT ***/
+
+void editor_draw_rows()
+{
+    for(int y = 0; y < ed_cfg.screen_rows; ++y)
+    {
+        write(STDOUT_FILENO, "~", 1);
+
+        if(y < ed_cfg.screen_rows - 1)
+        {
+            write(STDOUT_FILENO, "\r\n", 2);
+        }
+    }
+}
+
+void editor_refresh_screen()
+{
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
+    editor_draw_rows();
+
+    write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+/*** INPUT ***/
+
+void editor_process_keypress()
+{
+    char c;
+
+    c = editor_read_key();
+
+    switch(c)
+    {
+        case CTRL_KEY('q'):
+            write(STDOUT_FILENO, "\x1b[2J", 4);
+            write(STDOUT_FILENO, "\x1b[H", 3);
+            exit(0);
+        default:
+            break;
+    }
+}
+
 /*** INIT ***/
+
+void init_editor()
+{
+    if(get_window_size(&ed_cfg.screen_cols, &ed_cfg.screen_rows) == -1)
+        die("get_window_size");
+}
 int main()
 {
     enable_raw_mode();
+    init_editor();
 
     while(1) {
-        char c = '\0';
-        if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
-            die("read");
-        if (iscntrl(c)) {
-            printf("%d\r\n", c);
-        } else {
-            printf("%d (\'%c\')\r\n", c, c);
-        }
-        if (c == 'q') break;
+        editor_refresh_screen();
+        editor_process_keypress();
     }
 
     return 0;
